765fbc17b1aefa6795ff6c085138c62bfc49b901
Aaron Kern <aaronkern@Aarons-MacBook-Air.local>
2024-01-02 18:22:42 -0600

    Initial commit - setting up repo

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'retro_game_store',
+    password: '1234',
+    port: 5432,
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Retro Game Store API!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
